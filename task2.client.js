76c5fa9d9a1e27ae775ec5d5081e3705d874e989
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2017-12-28 17:47:07 +0300

    Fix change for client

--- task2.client.js
+++ task2.client.js
@@ -10,13 +10,16 @@ const hostPattern = /^(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])(\.(25[0
 const portPattern = /^\d{1, 4}$/;
 
 const socket = new net.Socket();
+let connection = false;
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', data => {
   data = data.substring(0, data.length -2);
-  commands.make(data);
+  if (!commands.make(data) && connection) {
+    socket.write(data);
+  }
 });
 
 commands.add('-help', 'Show help for program', () => {
@@ -30,45 +33,73 @@ commands.add('-configs', 'Show configs', () => {
 });
 
 commands.add('-set username', 'Set username', () => {
-  configs.username = readline.question('Set your username: ');
-  message.showSystemMessage('Username changed');
-  process.stdin.resume();
+  if(!connection) {
+    configs.username = readline.question('Set your username: ');
+    message.showSystemMessage('Username changed');
+    process.stdin.resume();
+  }
+  else {
+    message.showErrorMessage('Username changing is impossible');
+  }
 });
 
 commands.add('-set port', 'Set port', () => {
-  const res = readline.question('Set port: ');
-  if(portPattern.test(res)) {
-    configs.port = res;
-    message.showSystemMessage('Port changed');
+  if(!connection) {
+    const res = readline.question('Set port: ');
+    if(portPattern.test(res)) {
+      configs.port = res;
+      message.showSystemMessage('Port changed');
+    }
+    else {
+      message.showErrorMessage('New port is not valid');
+    }
+    process.stdin.resume();
   }
   else {
-    message.showErrorMessage('New port is not valid');
+    message.showErrorMessage('Port changing is impossible');
   }
-  process.stdin.resume();
 });
 
 commands.add('-set host', 'Set host', () => {
-  const res = readline.question('Set host: ');
-  if(hostPattern.test(res)) {
-    configs.host = res
-    message.showSystemMessage('Host changed');
+  if(!connection) {
+    const res = readline.question('Set host: ');
+    if(hostPattern.test(res)) {
+      configs.host = res
+      message.showSystemMessage('Host changed');
+    }
+    else {
+      message.showErrorMessage('New host is not valid');
+    }
+    process.stdin.resume();
   }
   else {
-    message.showErrorMessage('New host is not valid');
+    message.showErrorMessage('Username changing is impossible');
   }
-  process.stdin.resume();
 });
 
 commands.add('-connect', 'Connection with server', () => {
-  const { port, host } = configs;
-  socket.connect(port, host);
+  if(!connection) {
+    const { port, host } = configs;
+    socket.connect(port, host);
+  }
+  else {
+    message.showErrorMessage('Operation is impossible');
+  }
+});
+
+commands.add('-exit', 'Close programm', () => {
+  connection && socket.end();
+  process.stdin.end();
 });
 
 socket.on('connect', () => {
   message.showSystemMessage('Connection established');
+  connection = true;
+  socket.write(configs.username);
 }).on('data', data => {
-  console.log(data);
+  message.showMessage(data.toString());
 }).on('end', () => {
+  connection = false;
   message.showSystemMessage('Connection is broken');
 }).on('error', err => {
   if(err) {
@@ -76,7 +107,12 @@ socket.on('connect', () => {
       case 'ECONNREFUSED':
         message.showErrorMessage('Server is not found');
         break;
+      case 'ECONNRESET': 
+        message.showErrorMessage('Server is not answered');
+        connection 
+        break;
       default:
+        console.log(err);
         throw err;
     }
   }
@@ -84,31 +120,3 @@ socket.on('connect', () => {
 
 message.showInfoMessage('For connection to use -connect');
 message.showInfoMessage('For getting more information to use use -help');
-
-/*const net = require('net');
-
-const client = net.createConnection({ port: 3080 }, () => {
-  client.write(`Client ${ new Date().getTime() }`);
-  console.log('Server connected');
-}).on('data', data => {
-  console.log(data.toString());
-}).on('error', err => {
-  if (err) {
-    end();
-  }
-}).on('end', () => end);
-
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', data => {
-  client.write(data);
-});
-
-const end = () => {
-  console.log('Server was closed! Check ENTER.');
-  client.destroy();
-  process.stdin.end();
-  process.stdin.end();
-};
-*/
