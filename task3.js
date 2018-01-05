f790d5e82c012e4caf1bb821df591fa6bd23e09c
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-01-05 10:12:27 +0300

    Fix change for style errors

--- task3.js
+++ task3.js
@@ -5,51 +5,53 @@ const configs = require('./configs/database.json');
 const maps = dataWorker.create(configs);
 
 server.use('/', (req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("Hello world");
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello world');
   res.end();
 });
 
 server.use('/sudoku', (req, res) => {
   maps.getAll().then(json => {
     jsonRes(res, json);
-  }).catch(err => error(res));
+  }).catch(err => error(err, res));
 });
 
 server.get('/sudoku/:id', (req, res, data) => {
   maps.getOne(data.url.id).then(json => {
     jsonRes(res, json);
-  }).catch(err => error(res));
+  }).catch(err => error(err, res));
 });
 
 server.del('/sudoku/:id', (req, res, data) => {
   maps.remove(data.post.id).then(json => {
     jsonRes(res, json);
-  }).catch(err => error(res));
+  }).catch(err => error(err, res));
 });
 
 server.put('/sudoku/:id', (req, res, data) => {
   maps.create({ value: data.post.value }, data.post.id).then(json => {
     jsonRes(res, json);
-  }).catch(err => error(res));
+  }).catch(err => error(err, res));
 });
 
 server.post('/sudoku/:id', (req, res, data) => {
   maps.update({ value: data.post.value }, data.url.id).then(json => {
     jsonRes(res, json);
-  }).catch(err => error(res));
+  }).catch(err => error(err, res));
 });
 
 const jsonRes = (res, json) => {
-  res.writeHead(200, {"Content-Type": "application/json"});
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(json));
   res.end();
-}
+};
 
-const error = res => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("Error");
-  res.end();
-}
+const error = (err, res) => {
+  if (err) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Error');
+    res.end();
+  }
+};
 
 server.start(8888);
